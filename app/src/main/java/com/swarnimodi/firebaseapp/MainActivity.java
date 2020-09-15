8f6991aeed8ec774541fe71fd935776add05f64b
Swarnim Modi <modiswarnim@gmail.com>
2020-09-15 21:43:00 +0530

    Basic firebase authentication app

--- app/src/main/java/com/swarnimodi/firebaseapp/MainActivity.java
+++ app/src/main/java/com/swarnimodi/firebaseapp/MainActivity.java
@@ -2,14 +2,36 @@ package com.swarnimodi.firebaseapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void sign_out(MenuItem item) {
+        mAuth.signOut();
+        Intent i = new Intent(MainActivity.this, Sign_In_Page.class);
+        startActivity(i);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.overflow_menu, menu);
+        return true;
     }
 }
